f04865dcc2c05710cfac5c863e26970d9757eb5f
riyansha <81138314+riyansha@users.noreply.github.com>
2021-07-02 23:23:13 +0530

    Update weatherdag.py

--- weatherdag.py
+++ weatherdag.py
@@ -26,14 +26,15 @@ def load_data(ds, **kwargs):
         country     = str(doc['sys']['country'])
         lat         = float(doc['coord']['lat'])
         lon         = float(doc['coord']['lon'])
-        humid       = float(doc['main']['humidity'])
-        press       = float(doc['main']['pressure'])
-        min_temp    = float(doc['main']['temp_min']) - 273.15
-        max_temp    = float(doc['main']['temp_max']) - 273.15
-        temp        = float(doc['main']['temp']) - 273.15
+        humid       = int(doc['main']['humidity'])
+        press       = int(doc['main']['pressure'])
+        min_temp    = int((doc['main']['temp_min']-32)/1.8)
+        max_temp    = int((doc['main']['temp_max']-32)/1.8)
+        temp        = int((doc['main']['temp']-32)/1.8)
         weather     = str(doc['weather'][0]['description'])
         todays_date = datetime.now().date()
-
+        date_time  = datetime.now()
+        
         valid_data = True
         for valid in np.isnan([lat, lon, humid, press, min_temp, max_temp, temp]):
             if valid is False:
@@ -41,17 +42,18 @@ def load_data(ds, **kwargs):
                     break
 
         row = (city, country, lat, lon, todays_date, humid, press, min_temp,
-               max_temp, temp, weather)
+               max_temp, temp, weather, date_time)
 
         insert_cmd = """INSERT INTO weather_table
                     (city, country, latitude, longitude,
                      todays_date, humidity, pressure,
-                     min_temp, max_temp, temp, weather)
+                     min_temp, max_temp, temp, weather, date_time)
                      VALUES
-                     (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"""
+                     (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"""
 
         if valid_data is True:
                pg_hook.run(insert_cmd, parameters=row)
+
 
 
 
@@ -73,19 +75,20 @@ dag = DAG(
            start_date=datetime(2021,5,3)
          )
 
+#push_task = PythonOperator(
+   # task_id='push_task',
+   # python_callable=push_location,
+   # provide_context=True,
+   # dag=dag
+
 
 task1 = BashOperator(
     task_id='get_weather',
-    bash_command='python3 /c/Users/Admin/airflow/DAGS/src/getweather.py',
+    bash_command='python3 /c/Users/Admin/airflow/DAGS/src/getweather.py',                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
     retries=3,
     dag=dag
 )
 
-#task2 = BashOperator(
-    #task_id='sleep',
-    #bash_command='sleep 5',
-    #dag=dag
-#)
     
 task2 = PythonOperator(
     task_id='transform_load',
